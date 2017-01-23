76ffc8af9215497a1f3a00f712573e06a041e992
laeri <lars.wuethrich@hotmail.com>
2017-01-23 16:18:31 +0100

    Create window and draw circle to screen

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <stack>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const int SCREEN_WIDTH = 640;
+    const int SCREEN_HEIGHT = 640;
+    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Search Algorithms");
+    sf::CircleShape circle(10.f);
+    circle.setPosition({10,10});
+    circle.setFillColor(sf::Color::Red);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::Escape) {
+                        window.close();
+                    }
+                    break;
+                case sf::Event::KeyReleased:
+                    break;
+                case sf::Event::MouseButtonPressed:
+                    break;
+                case sf::Event::MouseButtonReleased:
+                    break;
+            }
+        }
+        window.clear();
+        window.draw(circle);
+        window.display();
+    }
     return 0;
 }
